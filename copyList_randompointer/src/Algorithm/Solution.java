22aa89319602bbcfc0d37b4f39f536b1e88b3122
zoe <mailbymzw@gmailc.om>
2017-11-29 20:46:57 +0800

    test

--- copyList_randompointer/src/Algorithm/Solution.java
+++ copyList_randompointer/src/Algorithm/Solution.java
@@ -1,7 +1,30 @@
 package Algorithm;
+/*
+ * A linked list is given such that each node contains an additional random pointer
+ * which could point to any node in the list or null.
+ * Return a deep copy of the list.
+
+
+ */
 
 public class Solution {
-    public RandomListNode copyRandomList(RandomListNode head){
+
+    public static void main(String[] args){
+        RandomListNode test = new RandomListNode(0);
+        for(int i =0; i< 9;i++){
+            RandomListNode newNode = new RandomListNode(i+1);
+            test.next = newNode;
+        }
+        while(test.next.next != null){
+            test.random = test.next.next;
+        }
+
+        RandomListNode copytest = copyRandomList(test);
+    }
+
+
+
+    public static RandomListNode copyRandomList(RandomListNode head){
         if(head == null) return null;
 
         //first scan, copy the list, inserting the new node under the old
