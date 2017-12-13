058b1eae15ace5b43a03252c9332d79fe00a775a
zoe <mailbymzw@gmail.com>
2017-12-13 16:04:09 +0800

    test

--- merge_two_list/src/Algorithm/Solution.java
+++ merge_two_list/src/Algorithm/Solution.java
@@ -2,8 +2,8 @@ package Algorithm;
 
 public class Solution {
     public static void main(String[] args){
-        int[] set1 = {1,2,3,4,5};
-        int[] set2 = {99,98,97,96,95};
+        int[] set1 = {1,2,12,32,35};
+        int[] set2 = {3,10,11,16};
 
         ListNode List1 = new ListNode(set1[0]);
         ListNode List2 = new ListNode(set2[0]);
@@ -13,6 +13,9 @@ public class Solution {
             ListNode newNode1 = new ListNode(set1[i]);
             p1.next = newNode1;
             p1 = p1.next;
+
+        }
+        for(int i = 1; i < set2.length; i++){
             ListNode newNode2 = new ListNode(set2[i]);
             p2.next = newNode2;
             p2 = p2.next;
@@ -28,12 +31,38 @@ public class Solution {
 
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 
+        //空指针判断
         if(l1 == null)
             return l2;
         else if(l2 == null)
             return l1;
 
-        ListNode head = new ListNode()
+        //头节点
+        ListNode head = new ListNode(0);
+        ListNode start = head;
+
+        while(l1 != null || l2 != null){
+
+            if(l1 != null && l2 != null && l1.val < l2.val){
+                ListNode newNode = new ListNode(l1.val);
+                start.next = newNode;
+                start = start.next;
+                l1 = l1.next;
+            }else if(l1 != null && l2 != null && l1.val >= l2.val){
+                ListNode newNode = new ListNode(l2.val);
+                start.next = newNode;
+                start = start.next;
+                l2 = l2.next;
+            }else if(l1 == null ){
+                start.next = l2;
+                l2 = null;
+            }else if(l2 == null){
+                start.next = l1;
+                l1 = null;
+            }
+
+        }
+        return head.next;
 
     }
 }
