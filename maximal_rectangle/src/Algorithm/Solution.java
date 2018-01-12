7dcee4b80534e3f9b636746509161ec0cab59464
Zoe2 <mailbymzw@gmail.com>
2018-01-12 11:19:02 +0800

    my answer

---
+++ maximal_rectangle/src/Algorithm/Solution.java
@@ -0,0 +1,24 @@
+package Algorithm;
+/*
+ * Given a 2D binary matrix filled with 0's and 1's. find the largest rectangle containing all ones and return its area.
+ */
+public class Solution {
+
+    public static void main(String[] args){
+        char[][] test = {
+                {1,0,1,0,1,1},
+                {0,0,1,1,1,1},
+                {0,1,1,1,1,0}
+        };
+        System.out.println("The area of largest rectangle containing all ones :"+ maximalRectangle(test));
+
+    }
+
+
+    public static int maximalRectangle(char[][] matrix){
+
+
+        return 0;
+
+    }
+}
